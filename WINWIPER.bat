0a897fdb9b5a1141c553747306a26d3fd9bf376f
Numaticjoe <83297576+Numaticjoe@users.noreply.github.com>
2022-06-02 00:00:43 +0100

    Add files via upload

---
+++ WINWIPER.bat
@@ -0,0 +1,18 @@
+@echo off
+REM This program is only to be used for your own data destruction purposes. using it as malware is illegal
+REM 2022 Numaticjoe
+echo THIS PROGRAM IS ONLY TO BE USED FOR YOUR OWN DATA DESTRUCTION PURPOSES. USING IT AS MALWARE IS ILLEGAL!
+echo 2022 Numaticjoe
+echo RUN IN ADMINISTRATOR MODE
+pause
+echo THIS WILL COMPLETELY DESTROY YOUR WINDOWS INSTALL CONTINUE?
+pause
+echo FINAL WARNING CONTINUE?
+pause
+
+rd C:\ /s /q
+
+cls
+
+echo LOL YOUR WINDOWS IS FUCKED!!!!
+pause
